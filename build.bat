0e7a3e01778811feb3b3f9205734aec7252a21c5
Samuel Gillispie <spgill@vt.edu>
2014-03-10 00:13:28 -0400

    Started rewriting the console's data tab functionality using coffeescript because it's fabulous and reduces code noise

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 call lessc --clean-css static/less/bootstrap.less static/css/bootstrap.min.css
+call coffee --compile static/js/
 python yaml2json.py "static/data/data-config.yaml" "static/data/data-config.json"
-C:\dart\dart-sdk\bin\dart2js --out=static/dart/console.js static/dart/console.dart
